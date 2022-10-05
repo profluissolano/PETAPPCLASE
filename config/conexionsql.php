7a985010dc7825ae06d8e9efdffa864048dcde6d
Luis solano <prof.luissolano@gmail.com>
2022-10-05 08:55:00 -0500

    creacion y conexion a la bd
    creamos el proyecto y el arcchivo de conexion a la base de datos

---
+++ config/conexionsql.php
@@ -0,0 +1,35 @@
+<?php 
+    class conexionsql{
+
+        //VARIABLES DE CONEXION
+        private $host = 'localhost';
+        private $bdname ='petapps';
+        private $username ='root';
+        private $pass='';
+        private $conexion;
+
+        //metodo que nos ayudara a conectarnos
+        public function conectar(){
+            $this->conexion =null;
+
+            try{
+                //conectar a la base de datos
+                $conexion = new PDO('mysql:host=localhost;dbname='.$this->bdname,
+                                $this->username, $this->pass);
+                foreach($conexion->query('SELECT * FROM especie') as $especie){
+                    echo 'NOMBRE DE ESPECIE: ' . $especie[1]. '</br>';
+                }
+            }catch(PDOException $e){
+                echo "Ocurrio un error al conectarme a la base de datos ";//. $e;
+            }
+
+        }
+
+
+
+
+
+    }
+
+
+?>
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php 
+include 'config/conexionsql.php';
+echo "<h1>ESPECIES REGISTRADAS</h1>  ";
+$varconex = new conexionsql;
+$varconex->conectar();
+
+?>
